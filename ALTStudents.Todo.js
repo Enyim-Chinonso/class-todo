528b6acc12219e1fc733eb1cafa1efe89949281f
unknown <enyimchinonso23@gmail.com>
2024-03-22 13:11:43 +0100

    add todo class

---
+++ ALTStudents.Todo.js
@@ -0,0 +1,8 @@
+const {Todo} = require('./Todo')
+
+
+constAltschoolTodoList = new Todo("Altstudent", 3)
+
+AltschoolTodoList.AddTodo("Learn javascript")
+AltschoolTodoList.AddTodo("Learn javascript")
+AltschoolTodoList.AddTodo("Learn javascript")
---
+++ classes.js
@@ -0,0 +1,71 @@
+// class Student {
+
+// constructor (name, age, hasPaid, date) {
+//     this.name = name;
+//     this.age = age;
+//     this.hasPaid = hasPaid;
+//     this.date = date;
+//   }
+
+//   printName (){
+//     return `The name of this ${this.name}`
+//   }
+
+//   printAge () {
+//     return ` The age of the student ${this.age}`
+//   }
+
+//   printEDonPay (day) {
+//     if (this.hasPaid) {
+    
+//     return `${this.name} don pay\n when: ${day}`
+// }else {
+//     return `${this.name} never pay`
+// }
+//   }
+//  }
+ 
+
+//  const student1 = new Student('Chinonso', 20, false);
+//  const student2 = new Student('Prince', 2, true);
+
+//  console.log(student1.printEDonPay())
+//  console.log(student2.printEDonPay('Tuesday'))
+
+
+
+//  const student1 = new Student('Chinonso', 20, false);
+
+
+// console.log(student1)
+// console.log(student1.printAge());
+// console.log(student1.printEDonPay());
+
+// Classes
+
+class Todo {
+  constructor(type, section) {
+    this.todoArr = [];
+    this.type = type;
+    this.section = section;
+  }
+
+  get TodoLists() {
+
+  }
+  AddTodo(todo) {
+    this.todoArr.push(todo);
+  }
+
+  module.export
+
+  editTodo(todoEdit, newTodo){
+    // map over the array with index.
+    const newTodoArr = this.todoArr.map((item, index)=  >)
+
+    // if todoItem == given
+
+    // add the new todo
+  }
+}
+
